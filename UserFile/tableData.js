224d30e5b37085d643ee4cb18b254857aacbf853
Yan Lin <example@example.com>
2021-10-09 13:34:39 +0800

    {"Message":"add","MessageType":2,"Modules":[{"State":1,"Type":8,"Name":"ApplicationSetting"},{"State":1,"Type":11,"Name":"UserService"},{"State":1,"Type":12,"Name":"CustomLibrary"},{"State":1,"Type":1,"Name":"PCPage/表1_列表页面"}],"Version":"v1"}

---
+++ UserFile/tableData.js
@@ -0,0 +1,72 @@
+﻿function Modify(){
+    Forguncy.modifyTablesData({
+        表1:{
+            addRows:[
+                {
+                    文本:"bbb",
+                    整数:1,
+                    日期:"2001/12/12",
+                    时间:"12:00:00",
+                    是_否:0
+                },
+                {
+                    文本:"ccc",
+                    整数:3,
+                    日期:"2011/11/11",
+                    时间:"16:00:00",
+                    是_否:0
+                }
+            ],
+            editRows:[
+                {
+                    primaryKey:{
+                        ID:2
+                    },
+                    values:{
+                        文本:"edit1",
+                        整数:11,
+                        日期:"2001/12/12",
+                        时间:"12:00:00",
+                        是_否:0
+                    }
+                },
+                {
+                    primaryKey:{
+                        ID:3
+                    },
+                    values:{
+                        文本:"edit2",
+                        整数:14,
+                        日期:"2001/12/12",
+                        时间:"15:00:00",
+                        是_否:1
+                    }
+                }
+            ],
+            deleteRows:[
+                {
+                    ID:4
+                },
+                {
+                    ID :5
+                }
+            ]
+        }
+    
+    }, 
+    function(data)
+    {
+        alert("成功");
+    }, 
+    function(errorMessage)
+    {
+        alert("失败:"+errorMessage);
+    });
+    
+    
+    
+}
+
+
+
+
